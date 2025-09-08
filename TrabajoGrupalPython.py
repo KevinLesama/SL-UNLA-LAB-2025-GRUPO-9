12d6b0adc626740049e5b6b1dc9fdafb4c6f384c
agusssrs <agustinmancini2@hotmail.com>
2025-09-08 19:44:53 -0300

    Cambios

--- TrabajoGrupalPython.py
+++ TrabajoGrupalPython.py
@@ -1,6 +1,6 @@
 print("Dios nos ayude a aprobar esta materia 🙏")
 
-
+#operaciones matematicas y un condicional
 suma = 2 + 5
 print (f"total: {suma}")
 print (suma)
@@ -12,5 +12,8 @@ if (suma < 3):
     print (f"el total es menor que 3 -> {suma}")
 else:
     print ("perfecto")
-    
-    
+
+if(multiplicacion == 16):
+    print("Salio todo bien 🙏👍")
+else:
+    print("rompiste la compu")
