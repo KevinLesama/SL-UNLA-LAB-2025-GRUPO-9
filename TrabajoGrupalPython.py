71d2676a9b672aa0b35aef428ca2c259b67789f4
agusssrs <agustinmancini2@hotmail.com>
2025-09-08 19:35:46 -0300

    multiplicacion

--- TrabajoGrupalPython.py
+++ TrabajoGrupalPython.py
@@ -5,4 +5,6 @@ suma = 2 + 3
 print (f"total: {suma}")
 print (suma)
 
-    
+multiplicacion = 4*4.
+print(f"4*4 = {multiplicacion}")
+print("yo te doy lo que tu quieras bailame en el tubo suaaaaaveeeeeeeeeeeee")
