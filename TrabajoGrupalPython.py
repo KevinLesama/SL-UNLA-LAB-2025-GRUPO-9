779e838bdf8f620c26b5116a06aa672e957ca484
NahuelGarcia205 <nahugar7020@gmail.com>
2025-09-08 19:40:05 -0300

    perfeccion

--- TrabajoGrupalPython.py
+++ TrabajoGrupalPython.py
@@ -1,10 +1,16 @@
 print("Dios nos ayude a aprobar esta materia 🙏")
 
 
-suma = 2 + 3
+suma = 2 + 5
 print (f"total: {suma}")
 print (suma)
 
 multiplicacion = 4*4.
 print(f"4*4 = {multiplicacion}")
-print("yo te doy lo que tu quieras bailame en el tubo suaaaaaveeeeeeeeeeeee")
+
+if (suma < 3):
+    print (f"el total es menor que 3 -> {suma}")
+else:
+    print ("perfecto")
+    
+    
