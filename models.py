0c6053f8669729223bdc542f5832938aead2dd58
Kevin Lesama <kevinsotolesama@gmail.com>
2025-09-19 16:48:35 -0300

    Actualizacion models, clase turnos

--- models.py
+++ models.py
@@ -1,15 +1,24 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 class Persona(Base):
     tablename="personas"
-    dni = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    dni = Column(Integer)
     nombre = Column(String)
     email = Column(String)
     telefono = Column(String)
     fecha_de_nacimiento = Column(String)
     habilitado = Column(String, default="si")
 
+class Turnos(Base):
+    tablename="turnos"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fecha = Column(String)
+    hora = Column(String)
+    estado = Column(String, default="pendiente")
+    persona_id = Column(Integer , ForeignKey('persona.id'))
 
 
 
