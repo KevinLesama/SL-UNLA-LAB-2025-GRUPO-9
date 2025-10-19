0179a794e3efb566e01beb8215995e1e930fec65
orionjaime-lgtm <orionjaimeadell@gmail.com>
2025-10-18 23:42:31 -0300

    meses_Español

--- utils.py
+++ utils.py
@@ -54,3 +54,9 @@ HORARIOS_VALIDOS = [
     "12:00", "12:30", "13:00", "13:30", "14:00", "14:30",
     "15:00", "15:30", "16:00"
 ]
+
+#hecho por Orion Quimey Jaime
+MESES_ESPANOL = [
+    "enero", "febrero", "marzo", "abril", "mayo", "junio",
+    "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+]
